cb5cef0a51be2a101593e2deaf7d2c052ff5032b
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-08-26 08:40:15 +0100

    configurando o easlin com style guide stander do google

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "njk");
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
 
 const checkAgeQueryParam = (req, res, next) => {
-  const { age } = req.query;
+  const { age } = req.query
 
   if (!age) {
-    return res.redirect("/");
+    return res.redirect('/')
   }
 
-  next();
-};
+  return next()
+}
 
-app.get("/", (req, res) => {
-  return res.render("start");
-});
+app.get('/', (req, res) => {
+  return res.render('start')
+})
 
-app.get("/major", checkAgeQueryParam, (req, res) => {
-  const { age } = req.query;
-  return res.render("major", { age });
-});
+app.get('/major', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+  return res.render('major', { age })
+})
 
-app.get("/minor", checkAgeQueryParam, (req, res) => {
-  const { age } = req.query;
-  return res.render("minor", { age });
-});
+app.get('/minor', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+  return res.render('minor', { age })
+})
 
-app.post("/check", (req, res) => {
-  const { age } = req.body;
+app.post('/check', (req, res) => {
+  const { age } = req.body
 
   if (age >= 18) {
-    return res.redirect(`/major?age=${age}`);
+    return res.redirect(`/major?age=${age}`)
   } else {
-    return res.redirect(`/minor?age=${age}`);
+    return res.redirect(`/minor?age=${age}`)
   }
-});
+})
 
-app.listen(3000);
+app.listen(3000)
